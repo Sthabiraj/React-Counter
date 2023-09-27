1ce3f67260508a239639dfaf568b94e4a2c07915
Sthabiraj <birajshrestha2007.com>
2023-09-27 11:44:33 +0545

    React counter added

---
+++ src/hooks/use-counter.js
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+
+const useCounter = (forwards = true) => {
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCounter(
+        forwards
+          ? (prevCounter) => prevCounter + 1
+          : (prevCounter) => prevCounter - 1
+      );
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [forwards]);
+
+  return counter;
+};
+
+export default useCounter;
